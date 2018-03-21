8ad8c317f4cbe4051ff8f6311f2b47a1006e48ee
funandyx <32208431+funandyx@users.noreply.github.com>
2018-03-21 22:33:53 +0200

    Add files via upload

---
+++ Performing Operations in PHP/variable.php
@@ -0,0 +1,13 @@
+<?<?php 
+
+$body_temp = 98.6 ;
+echo $body_temp ;
+
+echo "<p>Body temperature is $body_temp
+					degrees Fahrenheit " ;
+
+$body_temp = 37.0 ;
+
+echo "( $body_temp degrees Celsius )</p>" ;
+
+ ?>
